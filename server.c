d2c28210d9af9a545d06a15839e7c673b64dadc4
konrad <konradseime@gmail.com>
2024-09-21 02:03:38 +0200

    Add functionality to return files from requested path

--- server.c
+++ server.c
@@ -9,26 +9,32 @@
 #define PORT 8080
 #define INDEX_FILE "index.html"
 
-void sendHttp(int socket) {
-    char *header = "HTTP/1.1 200 =OK\r\n"
+void sendHttp(int socket, const char *path) {
+
+    printf("User wants: %s\n",path);
+    FILE *file = fopen(path, "r");
+    if (file == NULL){
+        char *header = "HTTP/1.1 200 =OK\r\n"
                     "Content-Type: text/html\r\n"
                     "Connection: close\r\n"
                     "\r\n";
-    send(socket,header,strlen(header),0);
-
-    
-    FILE *file = fopen(INDEX_FILE, "r");
-    if (file == NULL){
+        send(socket,header,strlen(header),0);
 
         char *content = "<html>"
                         "<head><title>Test</title></head>"
                         "<body>"
-                        "<h1>Could not read html file i am afriaid/h1>"
+                        "<h1>Could not find html file i am afriaid</h1>"
+                        "<h3>That means one of us is incompetent</h3>"
                         "</body>"
                         "</html>";
         send(socket,content,strlen(content),0);
         return;
     }
+    char *header = "HTTP/1.1 200 =OK\r\n"
+                    "Content-Type: text/html\r\n"
+                    "Connection: close\r\n"
+                    "\r\n";
+    send(socket,header,strlen(header),0);
 
     char buffer[1024];
     size_t bytes_read;
@@ -39,9 +45,28 @@ void sendHttp(int socket) {
 
     fclose(file);
 
+}
+
+void get_request(int socket) {
+    char buffer[1024];
+    read( socket, buffer, 1024);
+    buffer[strcspn(buffer, "\r\n")] = 0;
+
+    char method[10], path[100];
+
+    sscanf(buffer, "%s %s", method, path);
+
+    if (path[0] == '/') {
+        memmove(path, path+1, strlen(path));
+    }
+
+    if (strlen(path) == 0) {
+        sendHttp(socket, INDEX_FILE);
+        return;
+    }
+    sendHttp(socket, path);
 
 
-    
 }
 
 int getInput(int socket, struct sockaddr_in adress) {
@@ -112,7 +137,7 @@ int main(int argc, char const *argv[]) {
             exit(EXIT_FAILURE);
         }
         printf("Connection form %s on port %d\n", inet_ntoa((struct in_addr)adress.sin_addr), ntohs(adress.sin_port));
-        sendHttp(new_socket);
+        get_request(new_socket);
         close(new_socket);
 
         
